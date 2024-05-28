50e4e314f73382027ea7df0651560cfa7c7c3965
sabibi_k <sabina7350@gmail.com>
2024-05-28 23:24:24 +0300

    Fixed

--- src/test/java/page/DashboardPage.java
+++ src/test/java/page/DashboardPage.java
@@ -5,7 +5,6 @@ import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import data.DataHelper;
 
-import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
